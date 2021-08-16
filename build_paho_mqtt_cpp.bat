c0e5d03a3f04fa204745a42925e6ab7f633c11dd
FDA <filinov_dmirty@mail.ru>
2021-08-16 10:45:13 +0300

    123

---
+++ build_paho_mqtt_cpp.bat
@@ -0,0 +1,21 @@
+SET INSTALL_PATH=d:/fda/smart_home
+
+cd %INSTALL_PATH%/vendors/paho.mqtt.c
+cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%/libs/paho-mqtt-c -DPAHO_BUILD_STATIC=TRUE
+cmake --build build/ --target install
+cd %INSTALL_PATH%
+
+cd %INSTALL_PATH%/vendors/paho.mqtt.c
+cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%/libs/paho-mqtt-c
+cmake --build build/ --target install
+cd %INSTALL_PATH%
+
+cd %INSTALL_PATH%/vendors/paho.mqtt.cpp
+cmake -Bbuild -H. -DPAHO_BUILD_STATIC=TRUE -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%/libs/paho-mqtt-cpp -DPAHO_BUILD_SAMPLES=ON -DPAHO_WITH_SSL=OFF -DCMAKE_PREFIX_PATH=%INSTALL_PATH%/libs/paho-mqtt-c
+cmake --build build/ --target install
+cd %INSTALL_PATH%
+
+cd %INSTALL_PATH%/vendors/paho.mqtt.cpp
+cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%/libs/paho-mqtt-cpp -DPAHO_BUILD_SAMPLES=ON -DPAHO_WITH_SSL=OFF -DCMAKE_PREFIX_PATH=%INSTALL_PATH%/libs/paho-mqtt-c
+cmake --build build/ --target install
+cd %INSTALL_PATH%
