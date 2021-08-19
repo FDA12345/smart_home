b70da050892c177c4ab867ab017be14aa6e028fd
FDA <filinov_dmirty@mail.ru>
2021-08-19 08:02:13 +0300

    132

---
+++ sdk/build_boost_1_77_0.bat
@@ -0,0 +1,8 @@
+pushd .
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
+popd
+cd boost_1_77
+call bootstrap.bat
+rem b2.exe --build-type=complete -j6 debug-symbols=on numa=on stage
+b2.exe --build-type=complete -j6 debug-symbols=on stage
+cd ..
