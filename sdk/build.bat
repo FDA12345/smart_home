70622dc879d6de3e2a457f4669fa708f4cc234be
FDA <filinov_dmirty@mail.ru>
2021-08-19 07:57:54 +0300

    paho mqtt c now with version

--- sdk/build.bat
+++
@@ -1,3 +0,0 @@
-cmake -B paho.mqtt.c/build paho.mqtt.c
-cmake --build paho.mqtt.c/build --config Debug
-cmake --build paho.mqtt.c/build --config Release
---
+++ sdk/build_paho.mqtt.c_1.3.9.bat
@@ -0,0 +1,3 @@
+cmake -B paho.mqtt.c_1.3.9/build paho.mqtt.c_1.3.9
+cmake --build paho.mqtt.c_1.3.9/build --config Debug
+cmake --build paho.mqtt.c_1.3.9/build --config Release
