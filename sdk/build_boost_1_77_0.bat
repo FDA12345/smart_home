6deca61e17786e2ab593c99452ee8da5d9cacbe1
FDA <filinov_dmirty@mail.ru>
2021-08-19 12:20:11 +0300

    remove boost_1_77_0

--- sdk/build_boost_1_77_0.bat
+++
@@ -1,8 +0,0 @@
-pushd .
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
-popd
-cd "boost_1_77_0"
-call bootstrap.bat
-rem b2.exe --build-type=complete -j6 debug-symbols=on numa=on stage
-b2.exe --build-type=complete -j6 debug-symbols=on stage
-cd ..
