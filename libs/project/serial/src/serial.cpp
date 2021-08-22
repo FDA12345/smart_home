e93b1b152a73a226c4fa58c498f3d3e2ab50da7b
FDA <filinov_dmitry@mail.ru>
2021-08-22 16:53:51 +0300

    123

--- libs/project/serial/src/serial.cpp
+++ libs/project/serial/src/serial.cpp
@@ -1,10 +1,20 @@
 #include "serial.h"
 
+#include <boost/asio.hpp>
+
 namespace serial
 {
 
 class SerialImpl : public Serial
 {
+public:
+	SerialImpl()
+		: m_port(m_io)
+	{
+	}
+private:
+	boost::asio::io_service m_io;
+	boost::asio::serial_port m_port;
 };
 
 Ptr Create(const Params& params)
