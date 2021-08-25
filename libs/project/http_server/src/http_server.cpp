b48e88cc949c3f96fff16d5667682140c999e5f5
FDA <filinov_dmirty@mail.ru>
2021-08-25 08:37:45 +0300

    123

--- libs/project/http_server/src/http_server.cpp
+++ libs/project/http_server/src/http_server.cpp
@@ -7,7 +7,8 @@
 using namespace net_server;
 using namespace net_server::http;
 
-using tcp = boost::asio::ip::tcp;
+namespace ip = boost::asio::ip;
+using tcp = ip::tcp;
 
 
 class HttpServer : public net_server::Server
@@ -15,7 +16,6 @@ class HttpServer : public net_server::Server
 public:
 	HttpServer(const Params& params)
 		: m_params(params)
-		, m_acceptor(m_io)//, tcp::endpoint(tcp::v4(), 9898))
 	{
 	}
 
@@ -31,12 +31,53 @@ public:
 
 	bool Start() override
 	{
-		boost::system::error_code ec;
-		const auto& ep = tcp::endpoint(tcp::v4(), 9898);
-		m_acceptor.open(ep.protocol(), ec);
-		m_acceptor.bind(ep, ec);
-		m_acceptor.listen(-1, ec);
-		m_acceptor.accept(ec);// listen(-1, ec);
+		Stop();
+
+		while (true)
+		{
+			boost::system::error_code ec;
+
+			const auto& address = m_params.address.empty() ? "0.0.0.0" : m_params.address;
+			logINFO(__FUNCTION__, "starting http server at " << address << ":" << m_params.port);
+
+			const auto& addr = ip::make_address(address, ec);
+			if (ec)
+			{
+				logERROR(__FUNCTION__, "address " << address << " not parsed");
+				break;
+			}
+
+			const auto& ep = tcp::endpoint(addr, m_params.port);
+
+			m_io = std::make_unique<boost::asio::io_service>();
+			m_acceptor = std::make_unique<tcp::acceptor>(*m_io);
+
+			m_acceptor->open(ep.protocol(), ec);
+			if (ec)
+			{
+				logERROR(__FUNCTION__, "acceptor open error " << ec.message());
+				break;
+			}
+
+			m_acceptor->bind(ep, ec);
+			if (ec)
+			{
+				logERROR(__FUNCTION__, "acceptor bind error " << ec.message());
+				break;
+			}
+
+			m_acceptor->listen(boost::asio::socket_base::max_listen_connections, ec);
+			if (ec)
+			{
+				logERROR(__FUNCTION__, "acceptor listen error " << ec.message());
+				break;
+			}
+
+			return true;
+		}
+		//m_acceptor.accept(ec);// listen(-1, ec);
+
+		Stop();
 		return false;
 	}
 
@@ -48,8 +89,8 @@ private:
 	const Params m_params;
 	const logger::Ptr m_log = logger::Create();
 
-	boost::asio::io_service m_io;
-	boost::asio::ip::tcp::acceptor m_acceptor;
+	std::unique_ptr<boost::asio::io_service> m_io;
+	std::unique_ptr<tcp::acceptor> m_acceptor;
 };
 
 Ptr net_server::http::CreateServer(const Params& params)
