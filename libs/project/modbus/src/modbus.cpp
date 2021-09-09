818fbd575de061f14a12877fd962f90c3b71a340
FDA <filinov_dmirty@mail.ru>
2021-09-09 09:27:58 +0300

    123

--- libs/project/modbus/src/modbus.cpp
+++ libs/project/modbus/src/modbus.cpp
@@ -1,6 +1,35 @@
 #include "modbus.h"
 
-serial::modbus::Ptr serial::modbus::Create(const Params& params)
+using namespace serial;
+
+class ModbusImpl : public modbus::Modbus
 {
-	return nullptr;
+public:
+	ModbusImpl(const Params& params)
+		: m_serial(serial::Create(params))
+	{
+	}
+
+	bool ReadHoldingRegisters() override
+	{
+		return false;
+	}
+
+	bool WriteSingleRegister() override
+	{
+		return false;
+	}
+
+	bool WriteMultipleRegisters() override
+	{
+		return false;
+	}
+
+private:
+	serial::Ptr m_serial;
+};
+
+modbus::Ptr modbus::Create(const Params& params)
+{
+	return std::make_unique<ModbusImpl>(params);
 }
