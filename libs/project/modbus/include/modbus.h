a235ea4b517df6e9df94133d0ea25adfc6eef44b
FDA <filinov_dmirty@mail.ru>
2021-09-10 10:50:58 +0300

    123

--- libs/project/modbus/include/modbus.h
+++ libs/project/modbus/include/modbus.h
@@ -11,7 +11,10 @@ namespace serial
 		public:
 			virtual ~Modbus() = default;
 
-			virtual bool ReadHoldingRegisters() = 0; //0x03
+			virtual bool Open() = 0;
+			virtual void Close() = 0;
+
+			virtual bool ReadHoldingRegisters(uint8_t deviceAddress, uint16_t address, uint16_t total, uint16_t* dst) = 0; //0x03
 
 			virtual bool WriteSingleRegister() = 0; //0x06
 			virtual bool WriteMultipleRegisters() = 0; //0x10
--- libs/project/modbus/src/modbus.cpp
+++ libs/project/modbus/src/modbus.cpp
@@ -10,9 +10,55 @@ public:
 	{
 	}
 
-	bool ReadHoldingRegisters() override
+	bool Open() override
 	{
-		return false;
+		return m_serial->Open();
+	}
+
+	void Close() override
+	{
+		m_serial->Close();
+	}
+
+	bool ReadHoldingRegisters(uint8_t deviceAddress, uint16_t address, uint16_t total, uint16_t* dst) override
+	{
+
+//https://modbus.org/docs/Modbus_Application_Protocol_V1_1b.pdf
+#pragma pack(push)
+#pragma pack(1)
+		struct Request
+		{
+			uint8_t deviceAddress = 0;
+			uint8_t function = 0;
+			uint16_t address = 0;
+			uint16_t total = 0;
+			uint16_t crc;
+		};
+
+		struct ResponseHdr
+		{
+			uint8_t deviceAddress = 0;
+			uint8_t function = 0;
+			uint16_t bytesTotal = 0;
+		};
+#pragma pack(push)
+
+		constexpr uint16_t SERIAL_ADU_SIZE = 256;
+		constexpr uint16_t MAX_PDU_SIZE = SERIAL_ADU_SIZE - 1 - 1 - 1;
+		constexpr uint16_t MAX_REGS_TOTAL = MAX_PDU_SIZE >> 1;
+
+		if (total > MAX_REGS_TOTAL)
+		{
+			return false;
+		}
+
+		Request req = { 0 };
+		req.deviceAddress = deviceAddress;
+		req.function = 0x03;
+		req.address = address;
+		req.total = total;
+
+		return m_serial->Write(reinterpret_cast<const char*>(&req), sizeof(req)) == sizeof(req);
 	}
 
 	bool WriteSingleRegister() override
