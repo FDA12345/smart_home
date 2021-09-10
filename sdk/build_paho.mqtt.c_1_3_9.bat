56e1e431db9e41dafc8ebbb41e3c54e6cfc31267
FDA <filinov_dmitry@mail.ru>
2021-09-10 15:20:15 +0300

    x64 setup

--- sdk/build_paho.mqtt.c_1_3_9.bat
+++ sdk/build_paho.mqtt.c_1_3_9.bat
@@ -1,5 +1,9 @@
 SET PAHO_MQTT_C=paho.mqtt.c_1_3_9
 
-cmake -B %PAHO_MQTT_C%/build %PAHO_MQTT_C%
-cmake --build %PAHO_MQTT_C%/build --config Debug
-cmake --build %PAHO_MQTT_C%/build --config Release
+cmake -B %PAHO_MQTT_C%/build_x32 %PAHO_MQTT_C%
+cmake --build %PAHO_MQTT_C%/build_x32 --config Debug
+cmake --build %PAHO_MQTT_C%/build_x32 --config Release
+
+cmake -DCMAKE_GENERATOR_PLATFORM=x64 -B %PAHO_MQTT_C%/build_x64 %PAHO_MQTT_C%
+cmake --build %PAHO_MQTT_C%/build_x64 --config Debug
+cmake --build %PAHO_MQTT_C%/build_x64 --config Release
