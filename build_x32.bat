fabbdd6f871837251e3d1805631594316717c8c5
FDA <filinov_dmirty@mail.ru>
2021-09-11 06:24:47 +0300

    123

--- build_x32.bat
+++ build_x32.bat
@@ -1,4 +1,4 @@
 cmake -B build_x32
 
-cmake --build build_x32 --config Debug
-cmake --build build_x32 --config Release
+rem cmake --build build_x32 --config Debug
+rem cmake --build build_x32 --config Release
--- build_x64.bat
+++ build_x64.bat
@@ -1,4 +1,4 @@
 cmake -DCMAKE_GENERATOR_PLATFORM=x64 -B build_x64 
 
-cmake --build build_x64 --config Debug
-cmake --build build_x64 --config Release
+rem cmake --build build_x64 --config Debug
+rem cmake --build build_x64 --config Release
