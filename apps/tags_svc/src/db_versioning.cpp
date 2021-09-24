7ab9c0020ae231112bff6203e44118a9b940d1a5
FDA <filinov_dmirty@mail.ru>
2021-09-24 12:48:59 +0300

    123

--- apps/tags_svc/src/db_versioning.cpp
+++ apps/tags_svc/src/db_versioning.cpp
@@ -1,17 +1,77 @@
 #pragma once
 
-#include "db.h"
+#include "db_versioning.h"
 
 namespace db
 {
-	namespace tags
-	{
-		class DbVersion
-		{
-		public:
-			virtual ~DbVersion() = default;
+namespace versioning
+{
 
-			virtual bool Upgrade() = 0;
-		};
+class DbVersioningImpl : public DbVersioning
+{
+public:
+	DbVersioningImpl(db::Ptr&& db)
+		: m_db(std::move(db))
+	{
+		LoadVersion();
 	}
+
+	bool RegisterVersion(DbVersion::Ptr&& dbVersion) override
+	{
+		auto it = m_versions.find(dbVersion->Version());
+		if (it != std::end(m_versions))
+		{
+			return false;
+		}
+
+		m_versions.emplace(std::make_pair(dbVersion->Version(), std::move(dbVersion)));
+		return true;
+	}
+
+	uint64_t Version() const override
+	{
+		return 0;
+	}
+
+	std::string Description() const override
+	{
+		return "";
+	}
+
+	bool Upgrade() override
+	{
+		return false;
+	}
+
+	bool Upgrade(uint64_t version) override
+	{
+		return false;
+	}
+
+	bool Downgrade() override
+	{
+		return false;
+	}
+
+	bool Downgrade(uint64_t version) override
+	{
+		return false;
+	}
+
+private:
+	void LoadVersion()
+	{
+	}
+
+private:
+	db::Ptr m_db;
+	std::map<uint64_t, DbVersion::Ptr> m_versions;
+};
+
+DbVersioning::Ptr Create(db::Ptr&& db)
+{
+	return std::make_unique<DbVersioningImpl>(std::move(db));
+}
+
+}
 };
--- apps/tags_svc/src/db_versioning.h
+++ apps/tags_svc/src/db_versioning.h
@@ -4,14 +4,47 @@
 
 namespace db
 {
-	namespace tags
+	namespace versioning
 	{
+
 		class DbVersion
 		{
 		public:
+			using Ptr = std::unique_ptr<DbVersion>;
+
+		public:
 			virtual ~DbVersion() = default;
 
-			virtual bool Upgrade() = 0;
+			virtual uint64_t Version() const = 0;
+			virtual std::string Description() const = 0;
+
+			virtual bool CanUpgrade(const db::Ptr& db) const { return true; }
+			virtual bool CanDowngrade(const db::Ptr& db) const { return false; }
+
+			virtual bool Upgrade(const db::Ptr& db) = 0;
+			virtual bool Downgrade(const db::Ptr& db) = 0;
 		};
+
+		class DbVersioning
+		{
+		public:
+			using Ptr = std::unique_ptr<DbVersioning>;
+			static Ptr Create(db::Ptr&& db);
+
+		public:
+			virtual ~DbVersioning() = default;
+
+			virtual bool RegisterVersion(DbVersion::Ptr&& dbVersion) = 0;
+
+			virtual uint64_t Version() const = 0;
+			virtual std::string Description() const = 0;
+
+			virtual bool Upgrade() = 0;//downgrade to previous version
+			virtual bool Upgrade(uint64_t version) = 0;
+
+			virtual bool Downgrade() = 0;//downgrade to previous version
+			virtual bool Downgrade(uint64_t version) = 0;
+		};
+
 	}
-};
+}
