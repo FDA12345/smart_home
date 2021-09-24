596d7e0d3fbbb9c021eac60e6165f7959b5343a7
FDA <filinov_dmirty@mail.ru>
2021-09-24 11:57:51 +0300

    123

---
+++ apps/tags_svc/src/tags_db.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+#include "tags_db_ver_1.h"
+
+namespace db
+{
+namespace tags
+{
+
+class TagsDb
+{
+public:
+	bool Upgrade()
+	{
+	}
+};
+
+}//tags
+}//db
---
+++ apps/tags_svc/src/tags_db.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "db.h"
+
+namespace db
+{
+	namespace tags
+	{
+		struct Params
+		{
+		};
+
+		Ptr Create(const Params& params);
+	}
+};
